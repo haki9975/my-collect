eaf74447bc69155620e33fde38d84344d4334293
haki9975 <elias.robert.hakim@gmail.com>
2021-02-06 15:02:10 -0500

    commit tree 5b7a538e8c1a1022ca17001dd2f1e14d11cbd1e2

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
 
 
+def my_collect(array)  #block here
+    collection = []
+    if block_given?
+        i = 0
+        while i < array.length
+             collection << yield(array[i])
+             i = i + 1 
+        end
+    else
+        puts "No block given!"
+    end
+    collection
+end
+
+#can handle an empty selection without error message
+#yields correct element from given collection (languages)
+#returns new collection of modified elements
+#does not modify original collection
+#yields correct element from given collection
+#returns new collection of modified elements
+#does not modify original collection
